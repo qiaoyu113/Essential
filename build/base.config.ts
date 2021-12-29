e4e621b195d68d6621041c6ea512bc07622dbcb9
乔宇 <qy0803@yeah.net>
2021-12-29 15:55:07 +0800

    提交嗲吗

--- build/base.config.ts
+++ build/base.config.ts
@@ -2,9 +2,9 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Markdown from 'vite-plugin-md';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // 文档: https://vitejs.dev/config/
 export default defineConfig({
